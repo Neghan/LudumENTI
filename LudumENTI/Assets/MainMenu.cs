4714bbf36065bdc548c598f3906339e9fd0ba494
gyoza14 <seychey14@hotmail.com>
2019-12-02 21:51:41 +0100

    Main menu done + Enemy tag changed

---
+++ LudumENTI/Assets/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject credits_GO;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        credits_GO.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+
+    } 
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        credits_GO.SetActive(true);
+    }
+    public void CloseCredits()
+    {
+        credits_GO.SetActive(false);
+    }
+
+}
