14c95aa52107c557410c09e0187d56afec99fcf0
gyoza14 <seychey14@hotmail.com>
2019-12-06 00:05:56 +0100

    dios y belial añadidos + añgun cambio. Falta acabar el juego.

---
+++ LudumENTI/Assets/EndGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
